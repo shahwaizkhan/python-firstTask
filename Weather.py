e7fcd4f3dd1ef93b51b556bbe2b593ca47a16279
Shahwaizkhan <shahwaizali99@gmail.com>
2017-04-03 01:02:13 -0700

    Update Weather.py

--- Weather.py
+++ Weather.py
@@ -1,77 +1,75 @@
-
-"""
-The Purpose of this project is basically to generate report on the weather data of Lahore from 1996 to 2011.
-The data is present in bunch of text files.
-it will read those text files and perform operations.
-After performing operations (Finding min,max temperatures) it will print report
-
 """
 
+This project will display results extracted from weather data
 
-import csv     #CSV(comma seperated values) used to read data from txt files.
-import glob    # Finds all the pathnames matching a specified pattern
+files which are stored in project directory.
 
+"""
 
-max_humidity_dict = {} # Year is key and Minimum Humidity is value e.g 1998 : 20
-min_humidity_dict = {} # Year is key and Maximm Humidity is value e.g 1998 : 50
-max_temp_dict = {} # Year is key and Maximum Temp is value e.g 1998 : 47
-min_temp_dict = {} # Year is key and Minimum Temp is value e.g 1998 : 2
-full_date_dict = {} # Year is key and Full Date is value e.g 1998 : 1998-12-10
+import csv
+import glob
 
-
-
-
-
+max_temp_dict = {}  # Monthly maximum temp will store e.g 1998:45
+min_temp_dict = {}  # Monthly minimum temp will store e.g 1998:3
+min_humidity_dict = {}  # Monthly minimum humidity will store e.g 1998:31
+max_humidity_dict = {}  # Monthly maximum temp will store e.g 1998:100
+full_date_dict = {}  # Complete date will store e.g 1998:1998-2-3
 
 
 def main():
-
     """
-    Entry point of the program (Main Function) which will call other functions
-    we will pass path of directory where all weather files are located.
+    This is Main function
+    
     """
-
-    read_contents_of_file = []            #List to store data of txt file
-    List = []                            #List to store each row
-    dir_path = "E:\Shahwaiz Data\Semester 6\Python\Task-1\weatherdata.txt"
-    files = glob.glob(dir_path)
+    files = glob.glob("*.txt")
     for afile in files:
-        with open(afile) as File:
-            File.__next__()
-            File.__next__()
-            Read_contents_of_File = csv.reader(File)
+        read_contents_of_file = [] 
+        save_contents_of_file = []
+        
+        with open(afile) as file:
+            # skip first two lines
+            file.__next__()
+            file.__next__()
+            read_contents_of_file = csv.reader(file)
             for row in read_contents_of_file:
+                # iterate over a file lines
                 if row != 0:
-                    List.append(row)
-            List = List[:-1]
-
-
-
-    maximum_temperature = max_temp_calculation(List)
-    minimum_temperature = min_temp_calculation(List)
-    maximum_humidity = max_humidity_calculation(List)
-    minimum_humidity = min_humidity_calculation(List)
-
-    date_of_max_temperature = get_date_of_maxTemp(maximum_temperature)
-    date_of_min_temperature = get_date_of_minTemp(minimum_temperature)
-    date_of_max_humidity = get_date_of_maxHumidity(maximum_humidity)
-    date_of_min_humidity = get_date_of_minHumidity(minimum_humidity)
-
-
-
-
-#Functions which will be called form Main Function
+                    save_contents_of_file.append(row)
+            save_contents_of_file = save_contents_of_file[:-1] # skip last line
+        # Passing list to functions for calculations
+        maximum_temperature = max_temp_calculation(save_contents_of_file)
+        minimum_temperature = min_temp_calculation(save_contents_of_file)
+        maximum_humidity = max_humidity_calculation(save_contents_of_file)
+        minimum_humidity = min_humidity_calculation(save_contents_of_file)
+        # Getting dates of temperatures(Max, Min)  
+        date_of_max_temperature = get_date_of_maxTemp(maximum_temperature)
+        date_of_min_temperature = get_date_of_minTemp(minimum_temperature)
+        date_of_max_humidity = get_date_of_maxHumidity(maximum_humidity)
+        date_of_min_humidity = get_date_of_minHumidity(minimum_humidity)
 
 
 def get_date_of_maxTemp(max_temp):
+    """
+    This function will give date of 
+    Maximum temperature
+    
+    """
     if max_temp != 0:
         date = max_temp[0]
+        # Split the date so that we can use Year e.g 1996 as a key
         year = str(date)[:4]
         max_temp_of_month = max_temp[1]
+        
+        # If key:Year is already store in dictionary. So data of month is already stored
         if year in max_temp_dict.keys():
             temp = int(max_temp_of_month)
             temp_in_dic = int(max_temp_dict[year])
+            
             if temp > temp_in_dic:
+                # Compare the NewTemp with existed Temp
+                # If Temp is greater than existed Temp
+                # Replace it with Temp and Date in both dictionaries
+                # Remember: Year will be the key in all dictionaries
                 max_temp_dict[year] = max_temp_of_month
                 full_fate_dict[year] = date
             else:
@@ -82,38 +80,60 @@ def get_date_of_maxTemp(max_temp):
     else:
         pass
 
-
-
-
-
+    
 def get_date_of_minTemp(min_temp):
+    """
+    This function will give date of 
+    Minimum temperature
+    
+    """
     if min_temp != 0:
         date = min_temp[0]
+        # Split the date so that we can use Year e.g 1996 as a key
         year = str(date)[:4]
         min_temp_of_month = min_temp[1]
+        
+        # If key:Year is already store in dictionary. So data of month is already stored
         if year in min_temp_dict.keys():
             temp = int(min_temp_of_month)
             temp_in_dic = int(min_temp_dict[year])
+            
             if temp < temp_in_dic:
+                # Compare the NewTemp with existed Temp
+                # If Temp is Less than existed Temp
+                # Replace it with Temp and Date in both dictionaries
+                # Remember: Year will be the key in all dictionaries
                 min_temp_dict[year] = min_temp_of_month
+                full_fate_dict[year] = date
             else:
                 pass
         else:
             min_temp_dict[year] = min_temp_of_month
-
+            full_date_dict[year] = date
     else:
         pass
 
 
 def get_date_of_maxHumidity(max_humidity):
+    """
+    This function will give date of 
+    Maximum humidity
+    
+    """
     if max_humidity != 0:
         date = max_humidity[0]
+        # Split the date so that we can use Year e.g 1996 as a key
         year = str(date)[:4]
         maximum_humidity_of_month = max_humidity[1]
+        # If key:Year is already store in dictionary. So data of month is already stored
         if year in max_humidity_dict.keys():
             humidity = int(maximum_humidity_of_month)
             existing_humidity = int(max_humidity_dict[year])
+            
             if humidity > existing_humidity:
+                # Compare the NewHumidity with existed Humidity 
+                # If NewHumidity is greater than existed Humidity. 
+                # Replace it with NewHumidity 
                 max_humidity_dict[year] =  maximum_humidity_of_month
             else:
                 pass
@@ -124,14 +144,25 @@ def get_date_of_maxHumidity(max_humidity):
 
 
 def get_date_of_minHumidity(min_humidity):
+    """
+    This function will give date of 
+    Minimum humidity
+    
+    """
     if min_humidity != 0:
         date = min_humidity[0]
+        # Split the date so that we can use Year e.g 1996 as a key
         year = str(date)[:4]
         minimum_humidity_of_month = min_humidity[1]
+        # If key:Year is already store in dictionary. So data of month is already stored
         if year in min_humidity_dict.keys():
             humidity = int(minimum_humidity_of_month)
             existing_humidity = int(min_humidity_dict[year])
+            
             if humidity < existing_humidity:
+                # Compare the NewHumidity with existed Humidity 
+                # If NewHumidity is less than existed Humidity. 
+                # Replace it with NewHumidity 
                 min_humidity_dict[year] =  minimum_humidity_of_month
             else:
                 pass
@@ -141,54 +172,68 @@ def get_date_of_minHumidity(min_humidity):
         pass
 
 
-
-
-
-
 def max_temp_calculation(list_of_temp):
+    """
+    This function will calculate
+    Maximum temperature
+    
+    """
     max_temperatures_list = []
     i = list_of_temp[1]
     for i in range(len(list_of_temp)):
         max_temperatures_list = max_temperatures_list.append(list_of_temp[i])
         i = i+23
+        # If not exists
     if not max_temperatures_list:
         return 0
     else:
         max_temp = max(max_temperatures_list)
         max_temp_string = str(max_temp)
+        
     for item in range(len(list_of_temp)):
+        
         if list_of_temp[item][1] == max_temp_string:
             max_temperatures_list.append(list_of_temp[item][0])
             max_temperatures_list.append(max_temp_string)
             break
-
-
+    
     return max_temperatures_list
 
 
-
 def mix_temp_calculation(list_of_temp):
+    """
+    This function will calculate
+    Minimum temperature
+    
+    """
     min_temperatures_list = []
     i = list_of_temp[3]
     for i in range(len(list_of_temp)):
         min_temperatures_list = min_temperatures_list.append(list_of_temp[i])
         i = i+23
+        # If not exists
     if not min_temperatures_list:
         return 0
     else:
         min_temp = min(min_temperatures_list)
         min_temp_string = str(min_temp)
+        
     for item in range(len(list_of_temp)):
+        
         if list_of_temp[item][3] == min_temp_string:
             min_temperatures_list.append(list_of_temp[item][0])
             min_temperatures_list.append(min_temp_string)
             break
-
-
+            
     return min_temperatures_list
 
 
 def max_humidity_calculation(list_of_humidity):
+    """
+    This function will calculate
+    Maximum humidity
+    
+    """
     i = 7
     month_max_humidity = []
     max_list_of_humidity = []
@@ -197,7 +242,6 @@ def max_humidity_calculation(list_of_humidity):
         i = i+23
     if not max_list_of_humidity:
         return 0
-
     else:
         maximum_humidity = max(list_of_humidity)
         max_humidity_string = str(maximum_humidity)
@@ -210,6 +254,11 @@ def max_humidity_calculation(list_of_humidity):
 
 
 def min_humidity_calculation(list_of_humidity):
+    """
+    This function will calculate
+    Minimum humidity
+    
+    """
     i = 9
     month_min_humidity = []
     min_list_of_humidity = []
@@ -218,7 +267,6 @@ def min_humidity_calculation(list_of_humidity):
         i = i+23
     if not min_list_of_humidity:
         return 0
-
     else:
         minimum_humidity = min(list_of_humidity)
         min_humidity_string = str(minimum_humidity)
@@ -230,7 +278,6 @@ def min_humidity_calculation(list_of_humidity):
         return month_min_humidity
 
 
-
 def year_hottest_days(): 
      """ 
      it will show the the Hottest days with year and complete date 
@@ -240,7 +287,8 @@ def year_hottest_days():
            str("Date").rjust(10), 
            str("Temp").rjust(11), 
            "[Report#2][Hottest Days Report]".rjust(8) 
-           ) 
+           )
+    print("-" * 90) # 90 dashes
      # iterate over dictionary key @Year e.g 1996 and display it's value.Uses same key for two dictionaries 
      # e.g 1996:1996-12-1 and 1996:49 
     for i in full_date_dict.keys(): 
@@ -261,7 +309,7 @@ def annual_report():
           str("MINHumidity").rjust(7), 
           "[Report#1][Annual Report]".rjust(8) 
           ) 
-     
+     print("-" * 90) # 90 dashes
      # iterate over dictionary key @Year e.g 1996 and display it's value.Uses same key for all dictionaries 
     for i in full_date_dict.keys(): 
         print(str(i).rjust(6), 
@@ -271,8 +319,7 @@ def annual_report():
               str(min_humidity_dict[i]).rjust(9) 
                ) 
 
-
-
+# Take input from user. What type of report to display
 userChoice = str(input("1. For Annual Report\n"
                        "2. For Hottest Day\n"
                        "Please Enter Your Choice: "))
